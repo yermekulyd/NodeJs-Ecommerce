771d8d2c930b4bbb8f51aef92f099eda9f4fcd13
Didar <yermekulyd@gmail.com>
2023-06-14 22:58:21 +0600

    Adding authMiddleware which with the help of token can perform operations with the user

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -7,12 +7,15 @@ const {
   deleteaUser,
   updatedUser,
 } = require("../controller/userCtrl");
+
+const { authMiddleware } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 router.post("/register", createUser);
 router.post("/login", loginUserCtrl);
 router.get("/all-users", getallUser);
-router.get("/:id", getaUser);
+router.get("/:id", authMiddleware, getaUser);
 router.delete("/:id", deleteaUser);
 router.put("/:id", updatedUser);
+
 module.exports = router;
